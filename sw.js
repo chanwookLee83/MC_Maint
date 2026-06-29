12e9d21512f1b102a96e1a7ced363e0ff6f9c0ff
chanwookLee83 <cw2@kakao.com>
2026-06-29 09:40:54 +0900

    Update cache version from v71 to v72

--- sw.js
+++ sw.js
@@ -62,7 +62,7 @@ self.addEventListener('message', event => {
 });
 
 // ── 캐시 (버전 올림) ──
-const CACHE = 'mms-v71';
+const CACHE = 'mms-v72';
 const ASSETS = [
   './index.html',
   './manifest.json',
