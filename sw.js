e1297e20598c65b249cbe276fd1178fd404491db
chanwookLee83 <cw2@kakao.com>
2026-06-29 11:24:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -62,7 +62,7 @@ self.addEventListener('message', event => {
 });
 
 // ── 캐시 (버전 올림) ──
-const CACHE = 'mms-v74';
+const CACHE = 'mms-v75';
 const ASSETS = [
   './index.html',
   './manifest.json',
