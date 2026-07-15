b6efe60ddfe38a574c3438d772f9049d62f797ec
이찬욱 <cw2@kakao.com>
2026-07-15 13:05:42 +0900

    일정 구분에 공정감사 추가, sw 캐시 v101

--- sw.js
+++ sw.js
@@ -68,7 +68,7 @@ self.addEventListener('message', event => {
 });
 
 // ── 캐시 (버전 올림) ──
-const CACHE = 'mms-v100';
+const CACHE = 'mms-v101';
 const ASSETS = [
   './index.html',
   './manifest.json',
